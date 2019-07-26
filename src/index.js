b984b827921aab60811b86915be6c99424172f1d
Rachit Gupta <rachit371@gmail.com>
2019-07-26 10:36:46 +0530

    add new features

--- src/index.js
+++ src/index.js
@@ -1,70 +1,19 @@
 var rp = require("request-promise");
 let apiKey = null;
 
-function toTitleCase(str) {
-  return str.replace(/\w\S*/g, function(txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
-}
+// function toTitleCase(str) {
+//   return str.replace(/\w\S*/g, function(txt) {
+//     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//   });
+// }
 setApiKey = api_key => {
   apiKey = api_key;
 };
 
-getTrainRoute = (train_No, callback) => {
-  rp(`https://api.railwayapi.com/v2/route/train/${train_No}/apikey/${apiKey}/`)
-    .then(res => {
-      let stations = [];
-      let data = res.split('"route":')[1];
-      let data1 = data.split('"station":');
-      for (let i = 1; i < data1.length; i++) {
-        let name = data1[i].split('"name":')[1];
-        let code = data1[i].split('"code":')[1];
-
-        let scharr = data1[i].split('"scharr":')[1];
-        let schdep = data1[i].split('"schdep":')[1];
-        // console.log(name);
-        let final_name = name.split('"')[1];
-        let final_code = code.split('"')[1];
-        // console.log(schdep);
-
-        let final_scharr = scharr.split('"')[1];
-        const final_schdep = schdep.split('"')[1];
-        final_name = toTitleCase(final_name);
-        final_name = final_name.trim();
-        stations.push([final_name, final_code, final_scharr, final_schdep]);
-      }
-      callback(stations);
-    })
-    .catch(err => {
-      callback(err);
-    });
-};
-
-//
-getTrainBtwStation = (start, end) => {
+getTrainNoInformation = (train_No, callback) => {
   rp(
-    `https://api.railwayapi.com/v2/between/source/${start}/dest/${end}/apikey/${apiKey}/`
+    `http://indianrailapi.com/api/v2/TrainInformation/apikey/${apiKey}/TrainNumber/${train_No}/`
   )
-    .then(res => {
-      // for (let i = 1; i < data.length; i++) {
-      //   let name = data[i].split('"name":')[1];
-      //   console.log(name);
-      // }
-      let st = [];
-      let name = res.split('"trains":')[1];
-      // for (let i = 0; i < name.length; i++) {
-      //   let d = name[i].split('"travel_time":')[0];
-      //   console.log(d);
-      // }
-      callback(name);
-    })
-    .catch(err => {
-      callback(err);
-    });
-};
-
-getPnrStatus = (pnrNo, callback) => {
-  rp(`http://api.railwayapi.com/v2/pnr-status/pnr/${pnrNo}/apikey/${apiKey}/`)
     .then(res => {
       callback(res);
     })
@@ -72,4 +21,93 @@ getPnrStatus = (pnrNo, callback) => {
       callback(err);
     });
 };
-module.exports = { getTrainRoute, setApiKey, getTrainBtwStation };
+
+//-------------TrainBtwStation-----------//
+getTrainBtwStation = (start, end, callback) => {
+  rp(
+    `http://indianrailapi.com/api/v2/TrainBetweenStation/apikey/${apikey}/From/${start}/To/${end}`
+  )
+    .then(res => {
+      callback(res);
+    })
+    .catch(err => {
+      callback(err);
+    });
+};
+
+//-----------getLiveTrainStatus----------//
+getLiveTrainStatus = (train_No, date, callback) => {
+  rp(
+    `http://indianrailapi.com/api/v2/livetrainstatus/apikey/${apiKey}/trainnumber/${train_No}/date/${date}/`
+  )
+    .then(res => {
+      callback(res);
+    })
+    .catch(err => {
+      callback(err);
+    });
+};
+
+//--------TrainSchedule----------//
+getTrainSchedule = (train_No, callback) => {
+  rp(
+    `http://indianrailapi.com/api/v2/TrainSchedule/apikey/${apiKey}/TrainNumber/${train_No}/`
+  )
+    .then(res => {
+      callback(res);
+    })
+    .catch(err => {
+      callback(err);
+    });
+};
+
+//----------Train Fare-------//
+getTrainFare = (train_No, start, end, quota, callback) => {
+  rp(
+    `http://indianrailapi.com/api/v2/TrainFare/apikey/${apiKey}/TrainNumber/${train_No}/From/${start}/To/${end}/Quota/${quota}`
+  )
+    .then(res => {
+      callback(res);
+    })
+    .catch(err => {
+      callback(err);
+    });
+};
+
+//---------Seat Availability------//
+getSeatAvailability = (train_No, start, end, date, clas, callback) => {
+  rp(
+    `https://indianrailapi.com/api/v2/SeatAvailability/apikey/${apiKey}/TrainNumber/${train_No}/From/${start}/To/${end}/Date/${date}/Quota/GN/Class/${clas}`
+  )
+    .then(res => {
+      callback(res);
+    })
+    .catch(err => {
+      callback(err);
+    });
+};
+
+//---------Coach Position --------//
+
+getCoachPosition = (train_No, callback) => {
+  rp(
+    `http://indianrailapi.com/api/v2/CoachPosition/apikey/${apiKey}/TrainNumber/${train_No}/`
+  )
+    .then(res => {
+      callback(res);
+    })
+    .catch(err => {
+      callback(err);
+    });
+};
+
+module.exports = {
+  getTrainNoInformation,
+  setApiKey,
+  getTrainBtwStation,
+  getLiveTrainStatus,
+  getTrainSchedule,
+  getTrainFare,
+  getSeatAvailability,
+  getCoachPosition
+};
